fffc742423f2b9ace055eee9856ff433179c9957
solidheron <101203357+solidheron@users.noreply.github.com>
2025-04-24 00:07:51 -0500

    Update cosine_similarity.js

--- cosine_similarity.js
+++ cosine_similarity.js
@@ -6,7 +6,7 @@ chrome.storage.local.get(['cosine_similarity'], (result) => {
     const row = document.createElement('tr');
     row.innerHTML = `
       <td>${entry.similarity}</td>
-      <td><a href="${entry.url}" target="_blank">${entry.url}</a></td>
+      <td><a href="${entry.mod_url}" target="_blank">${entry.mod_url}</a></td>
     `;
     tbody.appendChild(row);
   });
