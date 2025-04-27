6d9a04046ef198756970b09d0446686e9182519d
solidheron <101203357+solidheron@users.noreply.github.com>
2025-04-27 01:12:26 -0500

    Update background.js

--- background.js
+++ background.js
@@ -9,8 +9,8 @@ const instances = [
 ];
 
 const instanceUrl = 'https://dalek.zone';
-const count = 100;
-const maxPages = 3;
+const count = 25;
+const maxPages = 6;
 async function processAllInstances() {
   for (const instance of instances) {
     await fetchVideoUUIDs(instance);
