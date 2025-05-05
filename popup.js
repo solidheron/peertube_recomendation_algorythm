e80d7e4da508b6f0ffdf964855d8a6b798d933d3
solidheron <101203357+solidheron@users.noreply.github.com>
2025-05-05 01:22:08 -0500

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("open-similarity").addEventListener("click", () => {
-    chrome.tabs.create({
-      url: chrome.runtime.getURL("cosine_similarity.html")
-    });
-  });
-});
